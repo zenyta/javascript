bf086853ee6bb1658772d58996b58680a2848332
zenyta <52040041+zenyta@users.noreply.github.com>
2019-06-20 21:53:34 +0700

    Update adsdetect.js

--- adsdetect.js
+++ adsdetect.js
@@ -1,25 +1,37 @@
-  $( document ).ready(function() {
-    console.log('document loaded');
-    function areAdsBlocked(callback) {
-      var URL = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-      fetch(URL, {
-        method: 'HEAD', 
-        mode: 'no-cors'
-      })
-      .then(response => callback(false)) //Response was received --> ads are NOT blocked
-      .catch(error => callback(true));   //No response           --> ads are blocked
+/**
+ * Detects if there are extensions for blocking ads
+ * currently active in the browser.
+ *
+ * How to use:
+ * $.adblockDetector.detect().done(function(adsEnabled){
+ *     if (!adsEnabled) {
+ *         //Ads are blocked
+ *     }
+ * });
+ * @version 1.0.2
+ */
+;(function($){
+    if ($.adblockDetector) {
+        console.error('AdBlock Detector instance exists already');
+        return;
     }
-    function checkAdBlocker() {
-      areAdsBlocked(function(isBlocked){
-        if(isBlocked){
-          $('#result').text('Please, Disabled Your Ad-Block For This Pages.');
-          $('#result').css('background-color', 'white');
-          // TODO: Add code here which handles if visitors block ads!
-        } else {
-          $('#result').text('Ads Allowed');
-          $('#result').css('background-color', 'white');
+
+    $.adblockDetector = {
+        detect: function() {
+            var dfd = new $.Deferred();
+            var adsEnabled = false;
+            var $dummy = $('<div class="adsbox">&nbsp;</div>').appendTo('body');
+
+            setTimeout(function () {
+                if ($dummy.height() > 0) {
+                    adsEnabled = true;
+                }
+                $dummy.remove();
+
+                dfd.resolve(adsEnabled);
+            }, 100);
+
+            return dfd;
         }
-      })
-    }
-    checkAdBlocker();
-  });
+    };
+})(jQuery);
